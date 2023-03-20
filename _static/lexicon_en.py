a6aee1bc59e4be41a10efc913970b634d8ff2082
le-nate <99023298+le-nate@users.noreply.github.com>
2023-03-20 12:30:21 +0100

    add: lexicons for multilingual functionality

---
+++ _static/lexicon_en.py
@@ -0,0 +1,147 @@
+from settings import SESSION_CONFIG_DEFAULTS
+
+
+class Lexicon:
+
+    # task
+    lex_starting_balances = "Starting Balances"
+    lex_interest_earned_last_period = "Interest Earned Last Month"
+    lex_total_cash = "Total Cash"
+    lex_ending_balances = "Ending Balances"
+    lex_savings_account = "Savings Account"
+    lex_stock = "Stock"
+    lex_unit = 'unit'
+    lex_units = "units"
+    lex_market_data = "Market Data"
+    lex_salary = "Salaire"
+    lex_catalog = "Catalog"
+    lex_food = "Food"
+    lex_price = "Price"
+    lex_my_cart = "My Cart"
+    lex_name = "Name"
+    lex_quantity = "Quantity"
+    lex_prix_total = "Total price"
+    lex_finalize_purchase = "Finalize Purchase"
+    lex_total = "Total"
+    lex_current_choiceConfidence = '''"How confident are you that you made the 
+    right decision this month?'''
+
+    lex_current_choiceConfidence_1 = '''"Not at all confident to have made the 
+    right decision'''
+
+    lex_current_choiceConfidence_5 = '''"Absolutely confident to have made the 
+    right decision'''
+
+    lex_slider_your_value = "Your estimate"
+
+    lex_warning_final_purch = f'''Warning! Your {lex_stock} balance is currently 0. 
+    If this is a mistake, please click Cancel and update {lex_my_cart}. If you do 
+    not have enough money, please click OK to end the game and proceed to the 
+    additional tasks.'''
+
+    lex_slider = "Slider"
+
+    # General expressions
+    lex_game = "Savings Game"
+    lex_I_do_not_know = "I do not know"
+    lex_yes = "Yes"
+    lex_no = "No"
+    lex_maybe = "Maybe"
+    lex_somewhat = "Somewhat"
+    lex_true = "True"
+    lex_false = "False"
+    lex_not_applicable = "Not applicable"
+    lex_more = "More"
+    lex_less = "Less"
+    lex_the_same_amount = "The same amount"
+    lex_more_money = "More money"
+    lex_less_money = "Less money"
+    lex_same_money = "The same amount of money"
+    lex_none_of_the_above = "None of the above"
+    lex_next = "Next"
+    lex_click_next = '''Click the Next button below to proceed.'''
+
+    lex_completed_questionnaire = '''You have completed the questionnaire. 
+    Please, click the Next button below to proceed.'''
+
+    lex_completed_game_round = '''You have now completed today's round of the 
+    game. Click the Next button below to proceed to the follow-up questions.'''
+
+    lex_completed_task_round = '''You have now completed today's round of the 
+    game and follow-up questions. Click the Next button below to proceed.'''
+
+    lex_slider = "Slider"
+
+    lex_sliders_instructions = f'''Click the blue bar to reveal the 
+    {lex_slider}. Drag it along the bar to select your estimate with either 
+    your mouse or the arrow keys on your keyboard.'''
+
+    lex_remunerated_questions = "Remunerated Questions"
+    lex_increased = "Increased"
+    lex_decreased = "Decreased"
+    lex_stayed_same = "Stayed the same"
+    lex_increase = "Increase"
+    lex_decrease = "Decrease"
+    lex_stay_same = "Stay the same"
+    lex_no_change = "No change"
+    lex_it_increases = "It increases."
+    lex_it_decreases = "It decreases."
+    lex_it_stays_the_same = "It stays the same."
+    lex_not_enough_info = "There is not enough information to determine the effect."
+    lex_instructions = "Instructions"
+    lex_correct = "That is correct."
+    lex_interest_rate = "Interest Rate"
+    lex_nominal_interest_rate = "Nominal Interest Rate"
+    lex_real_interest_rate = "Real Interest Rate"
+    lex_completed_so_far = "Completed so far"
+    lex_inflation_rate = "Inflation Rate"
+    lex_purchasing_power = "Purchasing Power"
+    lex_comprehension = "Comprehension"
+    lex_stock_up = f"Buy more than 1 {lex_unit}"
+    lex_save = "Save"
+    lex_buy_1 = f"Buy 1 {lex_unit}"
+    lex_buy_x = "Buy {} {}"
+    lex_buy_more_than = "Buy more than {} {}"
+
+    # Numeracy
+    lex_num_1 = '''Out of 1,000 people in a small town 500 are members of a 
+    choir. Out of these 500 members in the choir 100 are men. Out of the 500 
+    inhabitants that are not in the choir 300 are men. What is the probability 
+    that a randomly drawn man is a member of the choir? ________ (%)'''
+
+    lex_num_2a = '''Imagine we are throwing a five-sided die 50 times. On 
+    average, out of these 50 throws how many times would this five-sided die 
+    show an odd number (1, 3 or 5)? ______ out of 50 throws.'''
+
+    lex_num_2b = '''Imagine we are throwing a loaded die (6 sides). The 
+    probability that the die shows a 6 is twice as high as the probability of 
+    each of the other numbers. On average, out of these 70 throws how many times 
+    would the die show the number 6? ________out of 70 throws.'''
+
+    lex_num_3 = '''In a forest 20% of mushrooms are red, 50% brown and 30% 
+    white. A red mushroom is poisonous with a probability of 20%. A mushroom 
+    that is not red is poisonous with a probability of 5%. What is the 
+    probability that a poisonous mushroom in the forest is red? ________ (%)'''
+
+    lex_numatt_1 = '''On a scale of 1 to 5, how confident are you in your 
+    mathematical abilities?'''
+
+    lex_numatt_2 = '''On a scale of 1 to 5, how confident are you in your 
+    financial decision-making abilities?'''
+
+    lex_numatt_3 = '''On a scale of 1 to 5, how confident are you in your 
+    understanding of mathematics?'''
+
+    lex_numatt_4 = '''On a scale of 1 to 5, how confident are you in your 
+    understanding of probability?'''
+
+    lex_numatt_5 = '''On a scale of 1 to 5, how confident are you in your 
+    understanding of economics?'''
+
+    lex_numatt_not_confident = "Not confident at all"
+    lex_numatt_confident = "Absolutely confident"
+
+    # Error messages
+
+    lex_error_task_instructions_general = '''That is incorrect. Please, review 
+    the instructions and correct your answer.'''
---
+++ _static/lexicon_fr.py
@@ -0,0 +1,145 @@
+from settings import SESSION_CONFIG_DEFAULTS
+
+
+class Lexicon:
+
+    # task
+    lex_starting_balances = "Soldes de Départ"
+    lex_interest_earned_last_period = "Intérêts Perçus le Mois Précédent"
+    lex_total_cash = "Encaisse Totale"
+    lex_ending_balances = "Soldes de Clôture"
+    lex_savings_account = "Compte Épargne"
+    lex_stock = "Stock"
+    lex_unit = 'unité'
+    lex_units = "unités"
+    lex_market_data = "Données du Marché"
+    lex_salary = "Salaire"
+    lex_catalog = "Catalogue"
+    lex_food = "Aliments"
+    lex_price = "Prix"
+    lex_my_cart = "Mon Panier"
+    lex_name = "Nom"
+    lex_quantity = "Quantité"
+    lex_prix_total = "Prix Total"
+    lex_finalize_purchase = "Finaliser l’Achat"
+    lex_total = "Total"
+    lex_current_choiceConfidence = "Dans quelle mesure êtes-vous sûr d'avoir pris la bonne décision ce mois-ci ?"
+    lex_current_choiceConfidence_1 = "Pas du tout sûr d'avoir pris la bonne décision"
+    lex_current_choiceConfidence_5 = "Absolument sûr d'avoir pris la bonne décision"
+    lex_slider_your_value = "Votre estimation"
+    lex_warning_final_purch = '''ATTENTION VOUS N'ALLEZ PAS SURVIVRE !! 
+    • Le solde de votre stock est actuellement de 0.
+    • Si c'est une erreur, veuillez cliquer sur *Annuler* et mettre à jour votre panier. 
+    • Si vous n'avez pas assez d'argent, veuillez cliquer sur *OK* pour terminer le jeu et passer aux tâches supplémentaires.'''
+    lex_warning_final_purch_2 = '''****Êtes-vous sûr(e) de ne pas vouloir survivre ?****
+    • Cliquez sur "Annuler" pour changer votre panier.
+    • Cliquez sur "OK" si vous voulez ne pas survivre.'''
+
+    lex_slider = "Curseur"
+
+    # General terms
+    lex_game = "Jeu de l'Épargne"
+    lex_I_do_not_know = "Je ne sais pas"
+    lex_yes = "Oui"
+    lex_no = "Non"
+    lex_maybe = "Peut-être"
+    lex_somewhat = "En partie"
+    lex_true = "Vrai"
+    lex_false = "Faux"
+    lex_not_applicable = "Pas concerné"
+    lex_more = "Plus"
+    lex_less = "Moins"
+    lex_the_same_amount = "La même quantité"
+    lex_more_money = "Plus d'argent"
+    lex_less_money = "Moins d'argent"
+    lex_same_money = "Autant d'argent"
+    lex_nothing = "Rien"
+    lex_none_of_the_above = "Aucun de ces éléments"
+    lex_next = "Suivant"
+
+    lex_click_next = '''Cliquez sur le bouton "Suivant" ci-dessous 
+    lorsque vous serez prêt à poursuivre.'''
+
+    lex_completed_questionnaire = '''Vous avez maintenant terminé le 
+    questionnaire. Cliquez sur le bouton "Suivant" ci-dessous 
+    lorsque vous êtes prêt à continuer.'''
+
+    lex_completed_game_round = f'''Vous avez maintenant terminé le 
+    tour du jeu d’aujourd’hui. {lex_click_next}'''
+
+    lex_completed_task_round = f'''Vous avez maintenant terminé le 
+    tour du jeu d’aujourd’hui et les questions de suivi. 
+    <br><br>{lex_click_next}'''
+
+    lex_sliders_instructions = f'''Cliquez sur la barre bleue pour 
+    faire apparaître le {lex_slider}. Faîtes-le glisser le long 
+    de la barre pour sélectionner votre estimation avec votre souris 
+    ou les flèches de votre clavier.'''
+
+    lex_remunerated_questions = "Questions rémunérées"
+    lex_increased = "Augmenté"
+    lex_decreased = "Baissé"
+    lex_stayed_same = "Resté constant"
+    lex_increase = "Augmente"
+    lex_decrease = "Baisse"
+    lex_stay_same = "Reste constant"
+    lex_no_change = "Pas de changement"
+    lex_it_increases = "Il augmente."
+    lex_it_decreases = "Il baisse."
+    lex_it_stays_the_same = "Il reste constant."
+    lex_not_enough_info = "Il n'y a pas assez d'informations pour déterminer l'effet."
+    lex_instructions = "Instructions"
+    lex_correct = "C'est exact."
+    lex_interest_rate = "Taux d'Intérêt"
+    lex_nominal_interest_rate = "Taux d'Intérêt Nominal"
+    lex_real_interest_rate = "Taux d'Intérêt Réel"
+    lex_inflation_rate = "Taux d'Inflation"
+    lex_purchasing_power = "Pouvoir d'Achat"
+    lex_completed_so_far = "État d'avancement"
+    lex_comprehension = "Compréhension"
+    lex_stock_up = f"Acheter plus d'une {lex_unit}"
+    lex_save = f"Acheter 0 {lex_unit}"
+    lex_buy_1 = f"Acheter 1 {lex_unit}"
+    lex_buy_x = "Acheter {} {}"
+    lex_buy_more_than = "Acheter plus de {} {}"
+
+    # Numeracy
+    lex_num_1 = '''Sur les 1.000 habitants d'une petite ville, 500 sont membres 
+    d'une chorale. Sur ces 500 membres de la chorale, 100 sont des hommes. Sur 
+    les 500 habitants qui ne sont pas dans la chorale, 300 sont des hommes. 
+    Quelle est le pourcentage de chance qu'un homme tiré au hasard soit membre 
+    de la chorale ? ________ (%) <br><br> (Indiquez un nombre entier)'''
+
+    lex_num_2a = '''Imaginez que nous lançons 50 fois un dé à cinq faces. En 
+    moyenne, sur ces 50 lancers, combien de fois ce dé à cinq faces 
+    indiquera-t-il un nombre impair (1, 3 ou 5) ? ______ sur 50 lancers.'''
+
+    lex_num_2b = '''Imaginez que nous lançons un dé pipé (6 faces). Le pourcentage 
+    de chance que le dé indique un 6 est deux fois plus élevée que la probabilité 
+    de chacun des autres chiffres. En moyenne, sur ces 70 lancers, combien de 
+    fois le dé affichera-t-il le chiffre 6 ? ________ sur 70 lancers.'''
+
+    lex_num_3 = '''Dans une forêt, 20% des champignons sont rouges, 50% bruns 
+    et 30% blancs. Un champignon rouge est toxique avec un pourcentage de 
+    chance de 20%. Un champignon qui n'est pas rouge est toxique avec un 
+    pourcentage de chance de 5 %. Quelle est le pourcentage de chance qu'un 
+    champignon toxique de la forêt soit rouge ? ________ (%)'''
+
+    lex_numatt_1 = '''Sur une échelle de 1 à 5, quel est votre degré de 
+    confiance dans vos capacités mathématiques ?'''
+
+    lex_numatt_2 = '''Sur une échelle de 1 à 5, quel est votre degré de 
+    confiance dans vos capacités à prendre des décisions financières ?'''
+
+    lex_numatt_3 = '''Sur une échelle de 1 à 5, quel est votre degré de 
+    confiance dans votre compréhension des probabilités ?'''
+
+    lex_numatt_4 = '''Sur une échelle de 1 à 5, quel est votre degré de 
+    confiance dans votre compréhension de l'économie ?'''
+
+    lex_numatt_not_confident = "Pas du tout confiant"
+    lex_numatt_confident = "Absolument confiant"
+
+    # Error messages
+    lex_error_task_instructions_general = '''C'est inexact. Veuillez 
+    revoir les instructions et corriger votre réponse.'''
